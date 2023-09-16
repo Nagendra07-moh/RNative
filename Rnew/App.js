8b911071e20bf2ecb5d45715a21a541a09b2f029
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-09-16 14:41:18 +0530

    .

--- Rnew/App.js
+++ Rnew/App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View ,StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+const Tabs = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator>
+        <Tabs.Screen name='Home' component={Home}/>
+        <Tabs.Screen name='Login' component={Login}/>
+        <Tabs.Screen name='SignUp' component={SignUp}/>
+
+      </Tabs.Navigator>
+      <StatusBar backgroundColor="white" animated={true} />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+
+const Login = ()=>{
+  return(
+    <View style={Styles.mainView}>
+      <Text style={Styles.mainText}>Login</Text>
+    </View>
+  )
+}
+
+const SignUp = ()=>{
+  return(
+    <View style={Styles.mainView}>
+      <Text style={Styles.mainText}>SignUp</Text>
+    </View>
+  )
+}
+
+const Home = ()=>{
+  return(
+    <View style={Styles.mainView}>
+      <Text style={Styles.mainText}>Home</Text>
+    </View>
+  )
+}
+
+
+
+
+
+const Styles = StyleSheet.create({
+  mainView:{
+    flex:1,
+    justifyContent:'center',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'grey'
   },
+  mainText:{
+    fontSize:30,
+    color :'white',
+
+  }
 });
